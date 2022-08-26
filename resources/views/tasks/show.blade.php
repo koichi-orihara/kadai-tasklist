f056fa23577fead41d3231deb774acb53507dddd
koichi.orihara <koichi_orihara@gdo.co.jp>
2022-08-26 07:41:22 +0000

    Fixing redirects

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -22,7 +22,7 @@
     </table>
     
     {{-- メッセージ編集ページへのリンク --}}
-    {!! link_to_route('tasks.edit', 'このメッセージを編集', ['task' => $task->id], ['class' => 'btn btn-light']) !!}
+    {!! link_to_route('tasks.edit', 'このタスクを編集', ['task' => $task->id], ['class' => 'btn btn-light']) !!}
     {{-- メッセージ編集ページへのリンク --}}
     {!! link_to_route('tasks.index', 'タスク一覧に戻る', null, ['class' => 'btn btn-success']) !!}
     {{-- メッセージ削除フォーム --}}
@@ -32,7 +32,5 @@
 
         </div>
     </div>
-@else
-<h1>他人のタスクの詳細は閲覧できません。</h1>
 @endif
 @endsection
